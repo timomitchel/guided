73bc1a743eff72b030ec60ea312594a37ed13887
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-19 11:59:00 -0700

    first test for homepage passing with links and welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Custom Routes
+  root "welcome#index"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
+  #RESTful Routes
+  resources :guides, only: [:index]
+  resources :cities, only: [:index]
+  resources :users, only: [:new]
 end
--- spec/features/visitor_sees_homepage_spec.rb
+++ spec/features/visitor_sees_homepage_spec.rb
@@ -5,8 +5,13 @@ describe "visitor sees homepage with all correct elements" do
     before :each do 
       visit '/'
     end
-    it "sees the welcome nav bar and links to guides, guidees, and create account" do 
 
+    it "sees the welcome nav bar and links to guides, guidees, and create account" do 
+      expect(page).to have_content "Welcome to Guided. Adventure Awaits!"
+      expect(page).to have_link "Create Account"  
+      expect(page).to have_link "Login"  
+      expect(page).to have_link "Guides"  
+      expect(page).to have_link "Cities"  
     end
   end
 end
