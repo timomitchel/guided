fb712e70e4e472f059586d68b21584759290862e
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-19 16:53:02 -0700

    add validations to user and add user_role test and user test files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates_presence_of :username, :password
+  validates_uniqueness_of :username
 
-
+  has_secure_password
+  
+  enum role: ["default", "guide", "admin"]
 end
